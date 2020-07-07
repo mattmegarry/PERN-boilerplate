7fcef63aaffd39d50f65286fc353bf037d44f8b9
Matt Megarry <mattmegarry@gmail.com>
2020-07-07 06:45:21 +0100

    No duplicate signups

--- server/src/components/User/user.controller.js
+++ server/src/components/User/user.controller.js
@@ -5,16 +5,18 @@ import { hashSaltPassword } from "../../utils/auth";
 
 export const createUser = async (req, res, next) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  // SKIPPING INPUT VALIDATION - JUST PROTOTYPING!
+  // TO DO: SKIPPING INPUT VALIDATION - JUST PROTOTYPING!
 
   try {
     const hashedPassword = await hashSaltPassword(password);
-    const user = await User.create(email, hashedPassword);
-    res.locals.data = user;
+    await User.create(email, hashedPassword);
+    res.locals.data = {
+      message:
+        "Success! Please check your email and click the verification link."
+    };
   } catch (e) {
     console.log(e);
-    res.locals.data = { message: "Something went wrong" };
+    res.locals.data = { message: "Something went wrong." };
   }
   next();
 };
