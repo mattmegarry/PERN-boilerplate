e20a26739a83e1bb058818c692be82e16f3bf275
Matt Megarry <mattmegarry@gmail.com>
2020-07-14 19:10:38 +0100

    Basic client-side layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header-outer">
+        <div className="header">
+          <h1>My App</h1>
+          <button className="menu">MENU</button>
+        </div>
+      </div>
+      <div className="main-outer">
+        <div className="main">
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+        </div>
+      </div>
+      <div className="footer-outer">
+        <div className="footer">
+          <div>Footer info left</div>
+          <div>Footer info right</div>
+        </div>
+      </div>
+    </>
   );
 }
 
